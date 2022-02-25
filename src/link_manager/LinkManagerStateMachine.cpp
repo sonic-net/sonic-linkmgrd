a8ddff5089fc3cddf199647943165ddc4ef51fc8
Jing Zhang <zhangjing@microsoft.com>
2022-02-25 11:38:28 -0800

    Incrementing tolerance on mux state inconsistency  (#27)
    ### Description of PR
    
    Summary:
    Fixes # (issue)
    
    This PR is to handle ```config mux mode standby/active``` failure on healthy ports caused by unexpected mismatching mux probe response.
    
    What I firstly observed is a mux state probing right after a switchover can be mismatching with state db, and there was no additional switchover happened between the toggle and probe. Conducted an experiment and further proved that there is a time gap between xcvrd's toggle and the mux probe state change. 
    
    This can trigger an undesirable switchback, and eventually appears to be a failure of ```config mux mode```. 
    
    * Issue
      For example, expected state change sequence of switching to active is: 
        ```
        standby (Link Prober), standby (Local Mux State) 
        -> wait, wait
        -> wait, active (receive mux state notification, indicating toggle completes) 
        -> active, active (receive self's heartbeat)
        ```
      It's normal to receive previous heartbeat from peer during the process, so the sequence can also be:
        ```
        standby, standby 
        -> wait, wait 
        -> standby, wait (receive previous HB from peer before toggle completes) 
        -> standby, active (receive mux state notification, indicating toggle completes) 
        -> state inconsistency triggers mux state probing through I2C 
        -> mux probe response: active + receive self's heartbeat 
        -> active, active
        ```
      But since we have the time gap mentioned above, a probe response can unexpectedly be ```standby```, linkmgrd will trigger another switchover to standby. 
    
    * Fix
    Previous, no matter how linkmgrd enter ```standby, active``` or ```active, standby``` composite state, an immediate mux probe will be triggered. 
    Now, when entering ```standby, active``` or ```active, standby``` composite state, check: 
      * If entering by link prober state change, do mux probe immediately. 
         Cause with link prober state change, toggle on cable definitely happen already. 
      * If entering by mux state notification, start a 300 ms timer. 
         When timer expires, if inconsistency still occurs, do mux probe. 
    
    What caused the delay? We will need further investigation. This PR is a workaround for unblocking purpose. 
    
    sign-off: Jing Zhang zhangjing@microsoft.com
    
    ### Approach
    #### What is the motivation for this PR?
    To avoid toggle failure on healthy cables. 
    
    #### How did you do it?
    When receiving mux state change (from the switchover), hold for 300ms before mux state probing. 
    
    #### How did you verify/test it?
    Scripted 1000 switchovers on 24 ports. Can't reproduce the issue any more.

--- src/link_manager/LinkManagerStateMachine.cpp
+++ src/link_manager/LinkManagerStateMachine.cpp
@@ -933,7 +933,7 @@ void LinkManagerStateMachine::startMuxProbeTimer(uint32_t factor)
 //
 // ---> handleMuxProbeTimeout(boost::system::error_code errorCode);
 //
-// handle when LinkProber heartbeats were lost due link down, bad cable or server down
+// handle when LinkProber heartbeats were lost due link down, bad cable, server down or state mismatching
 //
 void LinkManagerStateMachine::handleMuxProbeTimeout(boost::system::error_code errorCode)
 {
@@ -942,7 +942,12 @@ void LinkManagerStateMachine::handleMuxProbeTimeout(boost::system::error_code er
     if (errorCode == boost::system::errc::success &&
         (ps(mCompositeState) == link_prober::LinkProberState::Label::Wait ||
          ms(mCompositeState) == mux_state::MuxState::Label::Unknown ||
-         ls(mCompositeState) == link_state::LinkState::Label::Down)) {
+         ls(mCompositeState) == link_state::LinkState::Label::Down ||
+         (ps(mCompositeState) == link_prober::LinkProberState::Label::Standby &&
+         ms(mCompositeState) == mux_state::MuxState::Label::Active) ||
+        (ps(mCompositeState) == link_prober::LinkProberState::Label::Active &&
+         ms(mCompositeState) == mux_state::MuxState::Label::Standby))
+    ) {
         CompositeState currState = mCompositeState;
         enterMuxWaitState(mCompositeState);
         LOGINFO_MUX_STATE_TRANSITION(mMuxPortConfig.getPortName(), currState, mCompositeState);
@@ -1065,8 +1070,18 @@ void LinkManagerStateMachine::LinkProberStandbyMuxActiveLinkUpTransitionFunction
 )
 {
     MUXLOGINFO(mMuxPortConfig.getPortName());
-    // Probe the MUX state as internal MUX state is active while LP reports standby link
-    enterMuxWaitState(nextState);
+
+    if ((ps(mCompositeState) != ps(nextState)) &&
+        (ps(nextState) == link_prober::LinkProberState::Label::Active ||
+         ps(nextState) == link_prober::LinkProberState::Label::Standby)) {
+        // If entering by link prober state change, probe mux state immediately. 
+        enterMuxWaitState(nextState);
+    } else {
+        // There can be a delay for hardware state change in switchovers.
+        // So if not entering by link prober state change, start a timer for mux state probing. 
+        // When timer expires and if link prober & mux state remain inconsistent, do mux state probing. 
+        startMuxProbeTimer();
+    }
 }
 
 //
@@ -1095,7 +1110,17 @@ void LinkManagerStateMachine::LinkProberActiveMuxStandbyLinkUpTransitionFunction
 {
     MUXLOGINFO(mMuxPortConfig.getPortName());
 
-    enterMuxWaitState(nextState);
+    if ((ps(mCompositeState) != ps(nextState)) &&
+        (ps(nextState) == link_prober::LinkProberState::Label::Active ||
+         ps(nextState) == link_prober::LinkProberState::Label::Standby)) {
+        // If entering by link prober state change, probe mux state immediately.     
+        enterMuxWaitState(nextState);
+    } else {
+        // There can be a delay for hardware state change in switchovers.
+        // So if not entering by link prober state change, start a timer for mux state probing. 
+        // When timer expires and if link prober & mux state remain inconsistent, do mux state probing. 
+        startMuxProbeTimer();
+    }
 }
 
 //
