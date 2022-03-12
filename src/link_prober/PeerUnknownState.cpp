2d81f1a238a6498488af917647c8d6170e3f3a5b
Longxiang Lyu <lolv@microsoft.com>
2022-03-12 09:21:19 +0000

    Addpeer link prober state `PeerActiveState` and `PeerUnknownState`
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

---
+++ src/link_prober/PeerUnknownState.cpp
@@ -0,0 +1,91 @@
+/*
+ *  Copyright 2021 (c) Microsoft Corporation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include "link_prober/PeerActiveState.h"
+#include "link_prober/PeerUnknownState.h"
+#include "link_prober/LinkProberStateMachine.h"
+
+#include "common/MuxLogger.h"
+
+namespace link_prober
+{
+
+//
+// ---> PeerUnknownState(LinkProberStateMachineBase &stateMachine, common::MuxPortConfig &muxPortConfig);
+//
+// class constructor
+//
+PeerUnknownState::PeerUnknownState(
+    LinkProberStateMachineBase &stateMachine,
+    common::MuxPortConfig &muxPortConfig
+) :
+    LinkProberState(stateMachine, muxPortConfig)
+{
+}
+
+//
+// ---> handleEvent(IcmpPeerActiveEvent &event);
+//
+// handle IcmpPeerActiveEvent from LinkProber
+//
+LinkProberState* PeerUnknownState::handleEvent(IcmpPeerActiveEvent &event)
+{
+    MUXLOGDEBUG(getMuxPortConfig().getPortName());
+
+    LinkProberStateMachineBase *stateMachine =
+        dynamic_cast<LinkProberStateMachineBase *> (getStateMachine());
+    LinkProberState *nextState;
+
+    if (++mPeerEventCount >= getMuxPortConfig().getPositiveStateChangeRetryCount()) {
+        nextState = dynamic_cast<LinkProberState *> (stateMachine->getPeerActiveState());
+    }
+    else {
+        nextState = dynamic_cast<LinkProberState *> (stateMachine->getPeerUnknownState());
+    }
+
+    return nextState;
+}
+
+//
+// ---> handleEvent(IcmpPeerUnknownEvent &event);
+//
+// handle IcmpPeerUnknownEvent from LinkProber
+//
+LinkProberState* PeerUnknownState::handleEvent(IcmpPeerUnknownEvent &event)
+{
+    MUXLOGDEBUG(getMuxPortConfig().getPortName());
+
+    LinkProberStateMachineBase *stateMachine = dynamic_cast<LinkProberStateMachineBase *> (getStateMachine());
+    LinkProberState *nextState = dynamic_cast<LinkProberState *> (stateMachine->getPeerUnknownState());
+
+    resetState();
+
+    return nextState;
+}
+
+//
+// ---> resetState();
+//
+// reset current state attributes
+//
+void PeerUnknownState::resetState()
+{
+    MUXLOGDEBUG(getMuxPortConfig().getPortName());
+
+    mPeerEventCount = 0;
+}
+
+} /* namespace link_prober */
