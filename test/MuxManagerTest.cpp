5acd11abea2fb934c4b258ced9486b5f5e7aa073
zjswhhh <zhangjing@microsoft.com>
2022-03-14 16:46:38 +0000

    update unit test

--- test/MuxManagerTest.cpp
+++ test/MuxManagerTest.cpp
@@ -227,6 +227,9 @@ void MuxManagerTest::createPort(std::string port)
     linkManagerStateMachine->setDecreaseIntervalFnPtr(
         boost::bind(&FakeLinkProber::decreaseProbeIntervalAfterSwitch, mFakeLinkProber.get(), boost::placeholders::_1)
     );
+    linkManagerStateMachine->setRevertIntervalFnPtr(
+        boost::bind(&FakeLinkProber::revertProbeIntervalAfterSwitchComplete, mFakeLinkProber.get())
+    );
 
     linkManagerStateMachine->mComponentInitState.set(0);
 
