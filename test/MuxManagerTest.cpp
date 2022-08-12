339be49e19f8b0b7f3a3d82491057579434c9953
Jing Zhang <zhangjing@microsoft.com>
2022-08-12 12:07:40 -0700

    wait for handler to be completed (#114)

--- test/MuxManagerTest.cpp
+++ test/MuxManagerTest.cpp
@@ -582,12 +582,12 @@ TEST_F(MuxManagerTest, DbInterfaceRaceConditionCheck)
     for (uint32_t i=0; i<TOGGLE_COUNT; i++) {
         postMetricsEvent("Ethernet0", mux_state::MuxState::Label::Active);
         setMuxState("Ethernet0", mux_state::MuxState::Label::Active);
-        
-        // wait 10ms for handler to be completed 
-        usleep(10000);
+
+        // wait for handlers to be completed 
+        while(mDbInterfacePtr->mSetMuxStateInvokeCount != i+1 || mDbInterfacePtr->mPostMetricsInvokeCount != i+1) {
+            usleep(10000);
+        }
         EXPECT_FALSE(mDbInterfacePtr->mDbInterfaceRaceConditionCheckFailure);
-        EXPECT_EQ(mDbInterfacePtr->mSetMuxStateInvokeCount, i+1);
-        EXPECT_EQ(mDbInterfacePtr->mPostMetricsInvokeCount, i+1);
     }
 
     terminate();
