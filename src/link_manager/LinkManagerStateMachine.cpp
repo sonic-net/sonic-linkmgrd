d893be9be2744ecd8bdbb0e3e102914d6ca32a55
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2023-02-22 12:55:44 +0800

    [active-standby] Toggle to standby if link down and config auto (#173)
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

--- src/link_manager/LinkManagerStateMachine.cpp
+++ src/link_manager/LinkManagerStateMachine.cpp
@@ -826,8 +826,17 @@ void LinkManagerStateMachine::handleMuxConfigNotification(const common::MuxPortC
             mSendSwitchActiveCommandCause = link_manager::LinkManagerStateMachine::SwitchCause::ConfigMuxMode;
             mSendPeerSwitchCommandFnPtr();
         } else {
-            mMuxStateMachine.setWaitStateCause(mux_state::WaitState::WaitStateCause::DriverUpdate);
-            mMuxPortPtr->probeMuxState();
+            LOGWARNING_MUX_STATE_TRANSITION(mMuxPortConfig.getPortName(), mCompositeState, mCompositeState);
+            if (ls(mCompositeState) == link_state::LinkState::Down &&
+                ms(mCompositeState) != mux_state::MuxState::Label::Standby) {
+                CompositeState nextState = mCompositeState;
+                switchMuxState(link_manager::ActiveStandbyStateMachine::SwitchCause::LinkDown, nextState, mux_state::MuxState::Label::Standby, true);
+                LOGWARNING_MUX_STATE_TRANSITION(mMuxPortConfig.getPortName(), mCompositeState, nextState);
+                mCompositeState = nextState;
+            } else {
+                mMuxStateMachine.setWaitStateCause(mux_state::WaitState::WaitStateCause::DriverUpdate);
+                mMuxPortPtr->probeMuxState();
+            }
         }
 
         updateMuxLinkmgrState();
