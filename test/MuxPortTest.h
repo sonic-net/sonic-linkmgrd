22f55a78c819a4bcd58fb53bfd8f865131e63df9
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2024-08-06 19:43:11 +0800

    Fix state machine handler race conditions introduced by `strand::wrap` (#257)
    Approach
    What is the motivation for this PR?
    As the subject.
    
    Work item tracking
    Microsoft ADO (number only): 28585413
    How did you do it?
    Post the handler directly to handler instead of calling it with strand::wrap
    
    How did you verify/test it?
    UT
    
    Any platform specific information?
    Documentation

---
+++ test/MuxPortTest.h
@@ -0,0 +1,56 @@
+/*
+ *  Copyright 2021 (c) Microsoft Corporation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef MUXPORTTEST_H_
+#define MUXPORTTEST_H_
+
+#include <memory>
+#include "gtest/gtest.h"
+
+#include "FakeMuxPort.h"
+#include "FakeLinkManagerStateMachine.h"
+
+namespace test
+{
+
+class MuxPortTest: public testing::Test
+{
+public:
+    MuxPortTest();
+    virtual ~MuxPortTest() = default;
+
+    void runIoServiceThreaded(uint32_t count = 3);
+    void stopIoServiceThreaded();
+
+    void SetUp() override;
+
+    void TearDown() override;
+
+public:
+    boost::asio::io_service mIoService;
+    std::unique_ptr<boost::asio::io_service::work> mWork;
+    boost::thread_group mThreadGroup;
+    common::MuxConfig mMuxConfig;
+    std::shared_ptr<FakeDbInterface> mDbInterfacePtr;
+    std::string mPortName = "EtherTest01";
+    uint16_t mServerId = 01;
+    FakeMuxPort mFakeMuxPort;
+};
+
+
+} /* namespace test */
+
+#endif /* MUXPORTTEST_H_ */
